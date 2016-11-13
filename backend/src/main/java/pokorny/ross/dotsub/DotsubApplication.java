625b231044562c33e3c766c28b8f9de6f30f685d
Ross Pokorny <ross.pokorny@nextcentury.com>
2016-11-13 15:28:44 -0500

    Add CORS support

--- backend/src/main/java/pokorny/ross/dotsub/DotsubApplication.java
+++ backend/src/main/java/pokorny/ross/dotsub/DotsubApplication.java
@@ -2,11 +2,21 @@ package pokorny.ross.dotsub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+import org.ebaysf.web.cors.CORSFilter;
 
 @SpringBootApplication
+@Configuration
 public class DotsubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DotsubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DotsubApplication.class, args);
+    }
+
+    @Bean
+    public CORSFilter corsFilter() {
+        return new CORSFilter();
+    }
 }
