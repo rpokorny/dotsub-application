41142437b2e35e3f739bdce6cedacb75d466c0f9
Ross Pokorny <ross.pokorny@nextcentury.com>
2016-11-13 15:49:35 -0500

    Add jersey debugging

--- backend/src/main/java/pokorny/ross/dotsub/JerseyConfig.java
+++ backend/src/main/java/pokorny/ross/dotsub/JerseyConfig.java
@@ -1,15 +1,20 @@
 package pokorny.ross.dotsub;
 
+import java.util.logging.Logger;
+
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
 import org.springframework.stereotype.Component;
 
-import org.glassfish.jersey.media.multipart.MultiPartFeature;
-
 @Component
 public class JerseyConfig extends ResourceConfig {
+
     public JerseyConfig() {
         register(FileResource.class);
         register(MultiPartFeature.class);
+
+        //property("jersey.config.server.tracing.type", "ALL");
+        //property("jersey.config.server.tracing.threshold", "VERBOSE");
     }
 }
